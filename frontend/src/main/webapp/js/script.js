35d943266753c956153c85d8cce4501f64df97a0
David Eriksson <davideriksson@swedenmail.com>
2016-01-16 15:09:26 +0100

    restcall to backend

--- frontend/src/main/webapp/js/script.js
+++ frontend/src/main/webapp/js/script.js
@@ -1,45 +1,48 @@
-$( document ).ready(function() {
+'use strict';
 
-jQuery('#vmap').vectorMap(
-{
-    map: 'world_en',
-    backgroundColor: '#a5bfdd',
-    borderColor: '#818181',
-    borderOpacity: 0.25,
-    borderWidth: 1,
-    color: '#f4f3f0',
-    //colors: map med land-kod (underscore) och hexkodad färg
-    enableZoom: true,
-    hoverColor: '#c9dfaf',
-    hoverOpacity: null,
-    normalizeFunction: 'linear',
-    scaleColors: ['#b6d6ff', '#005ace'],
-    selectedColor: '#c9dfaf',
-    selectedRegions: [],
-    showTooltip: true,
-    onRegionClick: function(element, code, region)
-    {
-        var message = 'You clicked "'
-            + region
-            + '" which has the code: '
-            + code.toUpperCase();
+$(document).ready(function() {
+    var default_color = "#8B0000";
 
-        alert(message);
-        //show info
-        //add movie button
-    },
-    onLoad: function(event, map)
-    {
-        //load data
-    },
-    onRegionOver: function(event, code, region)
-    {
-        //show info
-    },
-    onLabelShow: function(event, label, code)
-    {
-        //show info
-    },
-});
+    function renderMap() {
+        jQuery('#vmap').vectorMap({
+            map: 'world_en',
+            hoverOpacity: 0.7,
+            hoverColor: false,
+            onRegionClick: function(element, code, region) {
+                var message = 'You clicked "' + region + '" which has the code: ' + code.toUpperCase();
+
+                alert(message);
+                //show info
+                //add movie button
+            },
+            onLoad: function(event, map) {
+                //load data
+                $.ajax({
+                    url: 'http://localhost:10080/findCountries',
+                    type: 'GET',
+                    //data: 'ID=1&Name=John&Age=10', // or $('#myform').serializeArray()
+                    success: function(data) {
+                        var colors = {};
+                        for (var i in data) {
+                            colors[data[i].code] = default_color;
+                            var country = map.countries[data[i].code.toLowerCase()];
+                            if (country != undefined) {
+                                map.countries[data[i].code.toLowerCase()].setFill(default_color);
+                            }
+                        }
+                        console.log(Object.keys(colors).length);
+                    }
+                });
+            },
+            onRegionOver: function(event, code, region) {
+                //show info
+            },
+            onLabelShow: function(event, label, code) {
+                //show info
+            },
+        });
+    }
+
+    renderMap();
 
 });
