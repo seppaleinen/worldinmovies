4a7132646725a30508797b5a4a44b50181891f94
David Eriksson <david.eriksson@ehalsomyndigheten.se>
2016-01-26 16:18:51 +0100

    Started on ftpdownload from imdb  in batch

--- backend/batch/src/main/java/se/david/batch/job/imdb/MovieStep.java
+++ backend/batch/src/main/java/se/david/batch/job/imdb/MovieStep.java
@@ -13,14 +13,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
-import se.david.batch.job.imdb.beans.Helper;
-import se.david.batch.job.imdb.beans.ImdbItemReader;
 import se.david.batch.job.imdb.beans.ImdbItemWriter;
 import se.david.batch.job.imdb.beans.ImdbProcessor;
 import se.david.commons.Movie;
 
 import javax.sql.DataSource;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
 import java.nio.charset.StandardCharsets;
+import java.util.zip.GZIPInputStream;
 
 @Service
 @Log
@@ -29,14 +33,18 @@ public class MovieStep {
     private ImdbProcessor imdbProcessor;
     @Autowired
     private ImdbItemWriter imdbItemWriter;
-    @Autowired
-    private Helper helper;
-    @Autowired
-    private ImdbItemReader imdbItemReader;
+//    @Autowired
+//    private Helper helper;
+//    @Autowired
+//    private ImdbItemReader imdbItemReader;
 
     @Bean
     public ItemReader<String> reader() {
         FlatFileItemReader<String> reader = new FlatFileItemReader<>();
+
+        //InputStream inputStream = getInputStream();
+        //unzipFile(inputStream);
+        //reader.setResource(new PathResource(System.getProperty("user.home") + "/" + "countries.list"));
 
         reader.setResource(new ClassPathResource("countries.list"));
         LineMapper<String> lineMapper = new PassThroughLineMapper();
@@ -45,6 +53,53 @@ public class MovieStep {
         reader.setEncoding(StandardCharsets.ISO_8859_1.name());
 
         return reader;
+    }
+
+    private InputStream getInputStream() {
+        final String ftpUrl = "ftp://ftp.sunet.se/pub/tv+movies/imdb/countries.list.gz";
+
+        InputStream inputStream = null;
+
+        try{
+            URL url = new URL(ftpUrl);
+            URLConnection conn = url.openConnection();
+            inputStream = conn.getInputStream();
+        }
+        catch (IOException ex){
+            ex.printStackTrace();
+        } finally {
+            try {
+                if(inputStream != null) {
+                    inputStream.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return inputStream;
+    }
+
+    private void unzipFile(InputStream inputStream) {
+        String userPath = System.getProperty("user.home");
+
+        GZIPInputStream gZIPInputStream = null;
+        FileOutputStream fileOutputStream = null;
+        byte[] buffer = new byte[0];
+
+        try {
+            gZIPInputStream = new GZIPInputStream(inputStream);
+            fileOutputStream = new FileOutputStream(userPath + "/" + "countries.list");
+            int bytes_read;
+            while ((bytes_read = gZIPInputStream.read(buffer)) > 0) {
+                fileOutputStream.write(buffer, 0, bytes_read);
+            }
+            gZIPInputStream.close();
+            fileOutputStream.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Bean
