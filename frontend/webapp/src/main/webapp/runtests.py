f55d44f1d33257cd4fb96e2a2c8a35c03c092916
David Eriksson <davideriksson@swedenmail.com>
2016-05-29 13:09:51 +0200

    Make maven fail if python tests fail

--- frontend/webapp/src/main/webapp/runtests.py
+++ frontend/webapp/src/main/webapp/runtests.py
@@ -1,5 +1,6 @@
-import unittest
+import unittest, sys
 
 if __name__ == '__main__':
-	suite = unittest.TestLoader().discover('.', pattern = "test*.py")
-	unittest.TextTestRunner(verbosity=2).run(suite)
+    suite = unittest.TestLoader().discover('.', pattern = "test*.py")
+    ret = not unittest.TextTestRunner(verbosity=2).run(suite).wasSuccessful()
+    sys.exit(ret)
