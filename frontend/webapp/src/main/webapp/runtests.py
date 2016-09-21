1b32ff293c87ce59de2922c5ea9514d8f09b9c9d
David Eriksson <davideriksson@swedenmail.com>
2016-09-21 22:51:12 +0200

    python tests fix

--- frontend/webapp/src/main/webapp/runtests.py
+++ frontend/webapp/src/main/webapp/runtests.py
@@ -1,6 +1,7 @@
-import unittest, sys
+import unittest, sys, os
 
 if __name__ == '__main__':
-    suite = unittest.TestLoader().discover('.', pattern = "test*.py")
+    current_dir = os.path.dirname(__file__)
+    suite = unittest.TestLoader().discover(current_dir, pattern = "test*.py")
     ret = not unittest.TextTestRunner(verbosity=2).run(suite).wasSuccessful()
     sys.exit(ret)
