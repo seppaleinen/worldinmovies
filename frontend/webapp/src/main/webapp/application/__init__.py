cbc09fc505c269850cbca5fdae80d8c93decaa3c
David Eriksson <davideriksson@swedenmail.com>
2016-09-23 07:32:13 +0200

    Manually set dsn value for sentry

--- frontend/webapp/src/main/webapp/application/__init__.py
+++ frontend/webapp/src/main/webapp/application/__init__.py
@@ -5,4 +5,4 @@ app = Flask(__name__)
 app.debug=True
 
 app.config['SENTRY_RELEASE'] = os.environ.get('SENTRY_RELEASE', '0.0.1')
-sentry = Sentry(app)
+sentry = Sentry(app, dsn='https://34ce8ef3cc3740339cac1d70afae7d54:33f2eb87d7d8446cab3564f87d2be8fb@sentry.io/100439')
