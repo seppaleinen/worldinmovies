8b27fc3571588257a87a337b0cf6d722403b516b
David Eriksson <davideriksson@swedenmail.com>
2016-10-10 19:09:45 +0200

    Testing out bufferedstreamreader

--- backend/api/src/main/java/se/david/backend/controllers/services/ImportService.java
+++ backend/api/src/main/java/se/david/backend/controllers/services/ImportService.java
@@ -11,11 +11,9 @@ import se.david.backend.controllers.services.util.CountriesImporter;
 import se.david.backend.controllers.services.util.ImdbInterface;
 import se.david.backend.controllers.services.util.ImdbProcessor;
 
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.logging.Level;
-import java.util.stream.Stream;
 
 @Service
 @Log
@@ -34,8 +32,10 @@ public class ImportService {
 
     public void importImdbCountries() {
         Resource result = imdbInterface.getResource();
-        try (Stream<String> stream = Files.lines(Paths.get(result.getFile().getPath()), StandardCharsets.ISO_8859_1)) {
-            Iterators.partition(stream.skip(14).iterator(), 500).
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(result.getInputStream()));
+
+            Iterators.partition(reader.lines().skip(14).iterator(), 500).
                     forEachRemaining(batch -> movieRepository.save(imdbProcessor.process(batch)));
         } catch (Exception e) {
             log.log(Level.SEVERE, e.getMessage(), e);
