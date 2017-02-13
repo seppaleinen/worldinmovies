09c21848af01092bc8f0483b460af5c539663129
David Eriksson <davideriksson@swedenmail.com>
2017-02-13 13:33:03 +0100

    Moved Backend/api to backend, and moved packages

--- backend/api/src/main/java/se/david/backend/controllers/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package se.david.backend.controllers.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-import se.david.backend.controllers.repository.entities.User;
-
-@Repository
-public interface UserRepository extends MongoRepository<User, String> {
-}
