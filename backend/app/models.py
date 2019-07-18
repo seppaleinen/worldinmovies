4c8eb5baecf0681d37de3105660431f2ecdadb7f
David Eriksson <davideriksson@swedenmail.com>
2019-07-18 08:10:43 +0200

    Add index on primary keys

--- backend/app/models.py
+++ backend/app/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Movie(models.Model):
-    id = models.IntegerField(primary_key=True)
+    id = models.IntegerField(db_index=True, primary_key=True)
     original_title = models.CharField(max_length=500)
     popularity = models.DecimalField(decimal_places=3, max_digits=10)
     fetched = models.BooleanField(default=False)
@@ -25,25 +25,25 @@ class Movie(models.Model):
 
 
 class Genre(models.Model):
-    movie = models.ForeignKey(Movie, related_name='genres', on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, related_name='genres', on_delete=models.CASCADE, db_index=True)
     # id = models.IntegerField(primary_key=True)
     name = models.TextField()
 
 
 class AlternativeTitle(models.Model):
-    movie = models.ForeignKey(Movie, related_name='alternative_titles', on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, related_name='alternative_titles', on_delete=models.CASCADE, db_index=True)
     iso_3166_1 = models.CharField(max_length=20)
     title = models.CharField(max_length=500)
     type = models.CharField(max_length=100, blank=True, null=True)
 
 
 class SpokenLanguage(models.Model):
-    movie = models.ForeignKey(Movie, related_name='spoken_languages', on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, related_name='spoken_languages', on_delete=models.CASCADE, db_index=True)
     iso_639_1 = models.CharField(max_length=4)
     name = models.CharField(max_length=50)
 
 
 class ProductionCountries(models.Model):
-    movie = models.ForeignKey(Movie, related_name='production_countries', on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, related_name='production_countries', on_delete=models.CASCADE, db_index=True)
     iso_3166_1 = models.CharField(max_length=4)
     name = models.CharField(max_length=50)
