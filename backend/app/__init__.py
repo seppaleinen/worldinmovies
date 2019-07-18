7d4aae6b70c0b5edbc66953c22d5ad21f2a02a44
daveri <david.eriksson@avanza.se>
2019-07-18 15:31:37 +0200

    Add djangoIntegration to sentry

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,6 +1,7 @@
 import os, sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
 
 
 sentry_url = os.getenv('SENTRY_URL')
 if sentry_url:
-    sentry_sdk.init(sentry_url)
+    sentry_sdk.init(sentry_url, integrations=[DjangoIntegration()])
