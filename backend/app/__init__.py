9e2d5e597fcdb7349b92902491438fd2112bef98
daveri <david.eriksson@avanza.se>
2019-07-18 16:40:16 +0200

    Add release name to sentry integration

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -4,4 +4,4 @@ from sentry_sdk.integrations.django import DjangoIntegration
 
 sentry_url = os.getenv('SENTRY_URL')
 if sentry_url:
-    sentry_sdk.init(sentry_url, integrations=[DjangoIntegration()])
+    sentry_sdk.init(sentry_url, integrations=[DjangoIntegration()], release='backend-0.0.1')
