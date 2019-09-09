6bac55456b957acab2b1b083df32d3c0076a60db
David Eriksson <davideriksson@swedenmail.com>
2019-09-09 18:27:19 +0200

    Start of cypresstest importing imdb data

--- integration-tests/cypress/integration/test.spec.js
+++ integration-tests/cypress/integration/test.spec.js
@@ -49,4 +49,27 @@ context('Actions', () => {
     modal.get('tbody>tr').eq(1).get('td').eq(1).should('have.text', 'The Girl with the Dragon Tattoo')
     **/
   });
+
+  it('IMDB Ratings import should color code countries', () => {
+    cy.get('button:contains("Import")').click({force:true});
+    var importModal = cy.get('#importModal')
+        .should('be.visible')
+        .should('contain.text', 'Choose how you want to import your data')
+        .get('img[alt="IMDB"]').click({force:true});
+
+    cy.get('.import').should('contain.text', 'Import IMDB data');
+
+    cy.fixture('ratings.csv').as('csv')
+      .get('input[type=file]').then(function(el) {
+        return Cypress.Blob.base64StringToBlob(this.csv)
+          .then(blob => {
+            el[0].files[0] = blob
+            el[0].dispatchEvent(new Event('change', {bubbles: true}))
+          });
+    cy.get('path[data-code="SE"]').should('be.colored', '#c9dfaf');
+    cy.get('path[data-code="US"]').should('be.colored', '#A8A8A8');
+
+  })
+
+  })
 });
