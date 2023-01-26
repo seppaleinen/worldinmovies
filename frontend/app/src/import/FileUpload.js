9a742ab47e0f24970894b6b44d3ede2f9ae51116
daveri <david.eriksson@avanza.se>
2023-01-26 22:28:04 +0100

    Change loader to earth

--- frontend/app/src/import/FileUpload.js
+++ frontend/app/src/import/FileUpload.js
@@ -7,7 +7,7 @@ var FileUpload = inject("store")(
   observer(
     class FileUpload extends React.Component {
       onChangeHandler = (event) => {
-        document.getElementById("loader").style.display = "block";
+        document.getElementById("earth").style.display = "block";
         const data = new FormData()
         data.append('file', event.target.files[0])
         axios.post(process.env.REACT_APP_BACKEND_URL + "/ratings", data, {})
@@ -16,7 +16,7 @@ var FileUpload = inject("store")(
             this.props.store.myMovies = res.data.found;
           })
           .finally(() => {
-            document.getElementById("loader").style.display = "none";
+            document.getElementById("earth").style.display = "none";
             this.props.store.closeImportModal();
           });
       }
@@ -41,22 +41,7 @@ var FileUpload = inject("store")(
                   <input type="file" name="file" onChange={this.onChangeHandler}/>
                 </div>
 
-                <div id="loader">
-                    <div className="sk-circle">
-                        <div className="sk-circle1 sk-child"></div>
-                        <div className="sk-circle2 sk-child"></div>
-                        <div className="sk-circle3 sk-child"></div>
-                        <div className="sk-circle4 sk-child"></div>
-                        <div className="sk-circle5 sk-child"></div>
-                        <div className="sk-circle6 sk-child"></div>
-                        <div className="sk-circle7 sk-child"></div>
-                        <div className="sk-circle8 sk-child"></div>
-                        <div className="sk-circle9 sk-child"></div>
-                        <div className="sk-circle10 sk-child"></div>
-                        <div className="sk-circle11 sk-child"></div>
-                        <div className="sk-circle12 sk-child"></div>
-                    </div>
-                </div>
+                <div id="earth"></div>
             </div>
         )
       }
