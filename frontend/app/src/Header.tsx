1fb39329041d915fc73d3e217b72e58765e1b8f2
daveri <david.eriksson@avanza.se>
2023-03-23 10:45:59 +0100

    asd

--- frontend/app/src/Header.tsx
+++ frontend/app/src/Header.tsx
@@ -1,47 +1,47 @@
 import React from "react";
-import './Header.scss';
+import styles from './Header.module.scss';
 import {adminIcon, homeIcon, importMoviesIcon, searchIcon, worldMapIcon} from "./Svgs";
 
 const menuButton = () => {
-    return <div id={"menu"} className={"subnav"}>
-        <label className="hamb" htmlFor="side-menu"><span className="hamb-line"></span></label>
-        <label className={"button subnavbtn"} htmlFor="side-menu">
+    return <div id={"menu"} className={styles.subnav}>
+        <label className={styles.hamb} htmlFor="side-menu"><span className={styles.hambLine}></span></label>
+        <label className={styles.button} htmlFor="side-menu">
             <label htmlFor="side-menu">Menu</label>
         </label>
     </div>;
 }
 
 const adminButton = () => {
-    return <a href={"/admin"} className={"button"}>
+    return <a href={"/admin"} className={styles.button}>
         {adminIcon()}
         Admin
     </a>
 }
 
 const importMoviesButton = (props: Props) => {
-    return <div className={"button"} onClick={() => props.redirectToPage('import')}>
+    return <div className={styles.button} onClick={() => props.redirectToPage('import')}>
         {importMoviesIcon()}
         Import Movies
     </div>
 }
 
 const search = () => {
-    return <div className={"search button"}>
+    return <div className={`${styles.search} ${styles.button}`}>
         {searchIcon()}
-        <span className="input" role="textbox" contentEditable>
+        <span className={styles.input} role="textbox" contentEditable>
         </span>
     </div>;
 }
 
 const worldMapButton = (props: Props) => {
-    return <button className={"button worldmap"} onClick={() => props.redirectToPage('worldmap')}>
+    return <button className={`${styles.worldmap} ${styles.button}`} onClick={() => props.redirectToPage('worldmap')}>
         {worldMapIcon()}
         World Map
     </button>;
 }
 
 const homeButton = (props: Props) => {
-    return <button className={"button home"} onClick={() => props.redirectToPage('welcome')}>
+    return <button className={`${styles.button} ${styles.hoem}`} onClick={() => props.redirectToPage('welcome')}>
         {homeIcon()}
         The World in Movies
     </button>;
@@ -51,19 +51,19 @@ const homeButton = (props: Props) => {
 const Header = (props: Props) => {
     return (
         <header>
-            <div className={"left-headers"}>
-                <input className="side-menu" type="checkbox" id="side-menu"/>
+            <div className={styles.leftheaders}>
+                <input className={styles.sideMenu} type="checkbox" id="side-menu"/>
                 {menuButton()}
                 {homeButton(props)}
-                <nav>
+                <nav className={styles.nav}>
                     {worldMapButton(props)}
-                    <div className={"subnav-content"}>
+                    <div className={styles.subnavContent}>
                         {importMoviesButton(props)}
                         {adminButton()}
                     </div>
                 </nav>
             </div>
-            <div className={"right-headers"}>
+            <div className={styles.rightHeaders}>
                 {search()}
             </div>
         </header>
