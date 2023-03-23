da135164f9367751f0bb163f5f22ee78c45b8ae5
daveri <david.eriksson@avanza.se>
2023-03-23 20:45:10 +0100

    asd

--- frontend/app/src/movies/MovieDetails.tsx
+++ frontend/app/src/movies/MovieDetails.tsx
@@ -5,39 +5,64 @@ const MovieDetails = (props: Props) => {
     let movie = props.movie;
     return (
         <div className={styles.container}>
-            <img className={styles.poster} src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
-                 alt={movie.title}/>
-            <div>
+            <div className={styles.upperArea}>
+                <img className={styles.poster} src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
+                     alt={movie.title}/>
                 <div className={styles.upperTextArea}>
                     <div className={styles.titles}>
-                        <div>{movie.title} ({movie.release_date.slice(0, 4)})</div>
-                        <div className={`${styles.country} ${styles.button}`}>{movie.production_countries[0].name}</div>
+                        <div className={styles.title}>{movie.title} ({movie.release_date.slice(0, 4)})</div>
+                        <div className={styles.countries}>
+                            {movie.production_countries
+                                .map((item: any) => {
+                                    return item.iso_3166_1 === 'US' ? 'USA' : item.name
+                                })
+                                .map((name: string) => {
+                                    return <div className={`${styles.country} ${styles.button}`}>{name}</div>
+                                })}
+                        </div>
                     </div>
                     <div className={styles.plot}>{movie.overview}</div>
                     <div className={styles.bold}>
                         <h4>Director: </h4>
                         <div className={styles.notbold}>
-                            {movie.credits.crew.filter(function(p:any) {return p.job === 'Director'})
-                                .map((item:any) => {return <div key={item.name}>{item.name}</div>})}
+                            {movie.credits.crew.filter((p: any) => {
+                                return p.job === 'Director'
+                            })
+                                .map((item: any) => {
+                                    return <div key={item.name}>{item.name}</div>
+                                })}
                         </div>
                     </div>
                     <div className={styles.bold}>
                         <h4>Writers: </h4>
                         <div className={styles.notbold}>
-                            {movie.credits.crew.filter(function(p:any) {return p.department === 'Writing'})
-                                .map((item:any) => {return <div key={item.name}>{item.name}</div>})}
+                            {movie.credits.crew.filter((p: any) => {
+                                return p.department === 'Writing'
+                            })
+                                .map((item: any) => {
+                                    return <div key={item.name}>{item.name}</div>
+                                })}
                         </div>
                     </div>
                     <div className={styles.bold}>
                         <h4>Ratings: </h4>
                         <div className={styles.notbold}>
-                            {movie.vote_average}
+                            {Math.round(movie.vote_average)}/10
                         </div>
                     </div>
-                    <div className={`${styles.button}`}>I've seen it</div>
+                    <div className={`${styles.button} ${styles.seenit}`}>I've seen it</div>
                 </div>
             </div>
+
+            <div>{movie.runtime} minutes</div>
+
+            <div className={styles.genres}>
+                {movie.genres
+                    .map((a:any) => {return a.name})
+                    .map((a:any) => {return <div className={styles.button}>{a}</div>})}
+            </div>
         </div>
+
     )
 }
 
