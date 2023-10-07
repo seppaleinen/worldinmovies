049756c729c710930e6fb885ff082e8515b1eac5
daveri <david.eriksson@avanza.se>
2023-10-07 11:21:08 +0200

    asd

--- integration-tests/cypress/cypress.config.ts
+++ integration-tests/cypress/cypress.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'cypress'
 module.exports = defineConfig({
     e2e: {
         baseUrl: 'https://localhost',
-        specPattern: "cypress/integration/**/*.cy.{js,jsx,ts,tsx}"
+        specPattern: "cypress/integration/**/*.cy.{js,jsx,ts,tsx}",
+        experimentalRunAllSpecs: true
     }
 })
