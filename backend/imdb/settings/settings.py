c85e4713db349f90a6b6ae37d3d1093f6f5f04d0
daveri <david.eriksson@avanza.se>
2023-10-10 11:13:19 +0200

    Reduced tracing for sentry

--- backend/imdb/settings/settings.py
+++ backend/imdb/settings/settings.py
@@ -140,8 +140,8 @@ if sentryApi:
         # Set traces_sample_rate to 1.0 to capture 100%
         # of transactions for performance monitoring.
         # We recommend adjusting this value in production,
-        traces_sample_rate=1.0,
-        profiles_sample_rate=1.0,
+        traces_sample_rate=0.1,
+        profiles_sample_rate=0.1,
     )
 
 LANGUAGE_CODE = 'en-us'
